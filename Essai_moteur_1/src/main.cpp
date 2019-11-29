a53036cff29b12fc7cd4f302732e979c91fee3e9
lnibling <57904096+lnibling@users.noreply.github.com>
2019-11-29 08:54:11 +0100

    Add files via upload

---
+++ Essai_moteur_1/src/main.cpp
@@ -0,0 +1,84 @@
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+
+// Create the motor shield object with the default I2C address
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+// Or, create it with a different I2C address (say for stacking)
+// Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x61); 
+
+// Select which 'port' M1, M2, M3 or M4. In this case, M1
+Adafruit_DCMotor *myMotor = AFMS.getMotor(1);
+// You can also make another motor on port M2
+//Adafruit_DCMotor *myOtherMotor = AFMS.getMotor(2);
+
+void setup() {
+  Serial.begin(9600);           // set up Serial library at 9600 bps
+  Serial.println("Adafruit Motorshield v2 - DC Motor test!");
+
+  AFMS.begin();  // create with the default frequency 1.6KHz
+  //AFMS.begin(1000);  // OR with a different frequency, say 1KHz
+  
+  // Set the speed to start, from 0 (off) to 255 (max speed)
+  myMotor->setSpeed(150);
+  myMotor->run(FORWARD);
+  // turn on motor
+  myMotor->run(RELEASE);
+}
+
+void loop() {
+  // uint8_t i;
+  
+  // Serial.print("tick");
+
+  if (Serial.available() > 0) {
+    int content = Serial.read();
+
+    uint8_t mov = 150;
+    if (content == 'z') {
+      Serial.print("Forward");
+      myMotor->run(FORWARD);
+      mov++;
+      if (mov > 255) {
+        mov = 255;
+      }
+    } else if (content == 's') {
+      Serial.print("Backward");
+      myMotor->run(BACKWARD);
+      mov--;
+      if (mov < 0) {
+        mov = 0;
+      }
+    }
+
+    myMotor->setSpeed(mov);
+     myMotor->run(RELEASE);
+    delay(10);
+  }
+
+  // myMotor->run(FORWARD);
+  // for (i=0; i<255; i++) {
+  //   myMotor->setSpeed(i);  
+  //   delay(10);
+  // }
+  // for (i=255; i!=0; i--) {
+  //   myMotor->setSpeed(i);  
+  //   delay(10);
+  // }
+  
+  // Serial.print("tock");
+
+  // myMotor->run(BACKWARD);
+  // for (i=0; i<255; i++) {
+  //   myMotor->setSpeed(i);  
+  //   delay(10);
+  // }
+  // for (i=255; i!=0; i--) {
+  //   myMotor->setSpeed(i);  
+  //   delay(10);
+  // }
+
+  // Serial.print("tech");
+  // myMotor->run(RELEASE);
+  // delay(1000);
+  
+}
