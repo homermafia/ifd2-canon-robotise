d826536ee52b60f6be881e3e3841038935687dd1
lnibling <57904096+lnibling@users.noreply.github.com>
2019-12-13 08:07:44 +0100

    Add files via upload

--- Essai_moteur_1/src/main.cpp
+++ Essai_moteur_1/src/main.cpp
@@ -6,10 +6,10 @@ Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 // Or, create it with a different I2C address (say for stacking)
 // Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x61); 
 
-// Select which 'port' M1, M2, M3 or M4. In this case, M1
-Adafruit_DCMotor *myMotor = AFMS.getMotor(1);
-// You can also make another motor on port M2
-//Adafruit_DCMotor *myOtherMotor = AFMS.getMotor(2);
+// On prend sur port M1 l'actionneur horizontal
+Adafruit_DCMotor *Motor1 = AFMS.getMotor(1);
+// On prend sur M2 l'actionneur vertical
+Adafruit_DCMotor *Motor2 = AFMS.getMotor(2);
 
 void setup() {
   Serial.begin(9600);           // set up Serial library at 9600 bps
@@ -19,66 +19,74 @@ void setup() {
   //AFMS.begin(1000);  // OR with a different frequency, say 1KHz
   
   // Set the speed to start, from 0 (off) to 255 (max speed)
-  myMotor->setSpeed(150);
-  myMotor->run(FORWARD);
+  Motor1->setSpeed(150);
+  Motor1->run(FORWARD);
   // turn on motor
-  myMotor->run(RELEASE);
+  Motor1->run(RELEASE);
+
+  Motor2->setSpeed(150);
+  Motor2->run(FORWARD);
+  // turn on motor
+  Motor2->run(RELEASE);
 }
 
 void loop() {
-  // uint8_t i;
-  
-  // Serial.print("tick");
-
 int i=0;
-/*
-  if (Serial.available() > 0) {
-    int content = Serial.read();
 
-   uint8_t mov = 0;
-    if (content == 'z') {
-      Serial.print("Forward");
-      myMotor->run(FORWARD);
-      mov++;
-      if (mov > 255) {
-        mov = 255;
-      }
-    } else if (content == 's') {
-      Serial.print("Backward");
-      myMotor->run(BACKWARD);
-      mov--;
-      if (mov < 0) {
-        mov = 0;
-      }
-    }*/
-
-
-  
-
-   myMotor->run(FORWARD);
-   for (i=0; i<255; i++) {
-     myMotor->setSpeed(i);  
-     delay(20);
-   }
-   for (i=255; i!=0; i--) {
-     myMotor->setSpeed(i);  
-     delay(20);
-   }
-  
-   Serial.print("tock");
-
-   myMotor->run(BACKWARD);
-   for (i=0; i<255; i++) {
-     myMotor->setSpeed(i);  
-     delay(20);
-   }
-   for (i=255; i!=0; i--) {
-     myMotor->setSpeed(i);  
-     delay(20);
+   Motor1->run(FORWARD);
+   for (i=0; i<=255; i++) {
+     Motor1->setSpeed(i);  
+     delay(20); // a voir si modifier
    }
 
-   Serial.print("tech");
-   myMotor->run(RELEASE);
-   delay(1000);
-  
+   delay(3000); // modifier pour le temps de sortie du canon
+
+   for (i=255; i>=0; i--) {
+     Motor1->setSpeed(i);  
+     delay(20);
+   }   
+   Motor1->setSpeed(0);
+// lancer sequence : lever canon
+
+Motor2->run(BACKWARD);
+Motor2 ->setSpeed(150);       //monter
+delay(1000);
+
+Motor2->setSpeed(0);          //attendre
+delay(300);
+
+Motor2->run(FORWARD);         //descendre
+Motor2 ->setSpeed(150);
+delay(1500);
+
+Motor2->setSpeed(0);          //attendre
+delay(300);
+
+Motor2->run(BACKWARD);        //monter
+Motor2 ->setSpeed(150);
+delay(1000);
+
+Motor2->setSpeed(0);
+delay(1000);
+
+// jouer son de tir
+
+
+
+
+Motor1->run(BACKWARD);          // sequence de recul
+   for (i=255; i>=0; i=i-3) {
+     Motor1->setSpeed(i);  
+     delay(5);
+   }  
+   delay(10000);
+  Motor1->setSpeed(0);
+
+  Motor2->run(BACKWARD);          // sequence de recul
+   for (i=255; i>=0; i=i-3) {
+     Motor1->setSpeed(i);  
+     delay(5);
+   }  
+   delay(10000);
+  Motor2->setSpeed(0);
 }
